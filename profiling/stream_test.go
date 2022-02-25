48fcbfce5a9b9503a9c1afc980cffe67c0d2be62
mirkowang <mirkowang@tencent.com>
2022-02-25 20:17:30 +0800

    add micro profile kata

---
+++ profiling/stream_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func BenchmarkAlgorithmOne(b *testing.B) {
+	var output bytes.Buffer
+	in := assembleInputStream()
+	find := []byte("elvis")
+	repl := []byte("Elvis")
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		output.Reset()
+		algOne(in, find, repl, &output)
+	}
+}
+
+func BenchmarkAlgorithmTwo(b *testing.B) {
+	var output bytes.Buffer
+	in := assembleInputStream()
+	find := []byte("elvis")
+	repl := []byte("Elvis")
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		output.Reset()
+		algTwo(in, find, repl, &output)
+	}
+
+}
