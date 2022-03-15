c7713e7a14105b0f13de04694cf1c12304ff5352
mirkowang <mirkowang@mirkowangdeMacBook-Pro.local>
2022-03-16 02:35:33 +0800

    go-micro helloworld

---
+++ micro/helloworld/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"helloworld/handler"
+	pb "helloworld/proto"
+
+	"go-micro.dev/v4"
+	log "go-micro.dev/v4/logger"
+)
+
+var (
+	service = "helloworld"
+	version = "latest"
+)
+
+func main() {
+	// Create service
+	srv := micro.NewService(
+		micro.Name(service),
+		micro.Version(version),
+	)
+	srv.Init()
+
+	// Register handler
+	pb.RegisterHelloworldHandler(srv.Server(), new(handler.Helloworld))
+
+	// Run service
+	if err := srv.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
