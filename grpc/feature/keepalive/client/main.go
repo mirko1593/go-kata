9cb4208fe69bb5a7833d019c8b48725629f0d0cf
mirkowang <mirkowang@mirkowangdeMacBook-Pro.local>
2022-03-19 14:26:53 +0800

    grpc feature keepalive

---
+++ grpc/feature/keepalive/client/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"feature/api"
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/keepalive"
+)
+
+var addr = flag.String("addr", "localhost:50052", "the address to connect to")
+
+var kacp = keepalive.ClientParameters{
+	Time:                10 * time.Second, // send pings every 10 seconds if there is no activity
+	Timeout:             time.Second,      // wait 1 second for ping ack before considering the connection dead
+	PermitWithoutStream: true,             // send pings even without active streams
+}
+
+func main() {
+	flag.Parse()
+
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithKeepaliveParams(kacp))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := api.NewEchoClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	fmt.Println("Performing unary request")
+	res, err := c.UnaryEcho(ctx, &api.EchoRequest{Message: "Keepalive demo"})
+	if err != nil {
+		log.Fatalf("unexpected error from UnaryEcho: %v", err)
+	}
+
+	fmt.Println("RPC response: ", res)
+	// Block forever; run with GODEBUG=http2debug=2 to observe ping frames and GOAWAYs due to idleness.
+	select {}
+}
---
+++ grpc/feature/keepalive/server/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"feature/api"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
+)
+
+var port = flag.Int("port", 50052, "port number")
+
+var kaep = keepalive.EnforcementPolicy{
+	MinTime:             5 * time.Second, // If a client pings more than once every 5 seconds, terminate the connection
+	PermitWithoutStream: true,            //  Allow pings even when there are no active streams
+}
+
+var kasp = keepalive.ServerParameters{
+	MaxConnectionIdle:     15 * time.Second, // If a client is idle for 15 seconds, send a GOAWAY
+	MaxConnectionAge:      30 * time.Second, // If any connection is alive for more than 30 seconds, send a GOAWAY
+	MaxConnectionAgeGrace: 5 * time.Second,  // Allow 5 seconds for pending RPCs to complete before forcibly closing connections
+	Time:                  5 * time.Second,  // Ping the client if it is idle for 5 seconds to ensure the connection is still active
+	Timeout:               1 * time.Second,  // Wait 1 second for the ping ack before assuming the connection is dead
+}
+
+type server struct {
+	api.UnimplementedEchoServer
+}
+
+func (s *server) UnaryEcho(ctx context.Context, in *api.EchoRequest) (*api.EchoResponse, error) {
+	return &api.EchoResponse{Message: in.Message}, nil
+}
+
+func main() {
+	flag.Parse()
+
+	address := fmt.Sprintf(":%v", *port)
+	lis, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer(grpc.KeepaliveEnforcementPolicy(kaep), grpc.KeepaliveParams(kasp))
+	api.RegisterEchoServer(s, new(server))
+
+	s.Serve(lis)
+}
