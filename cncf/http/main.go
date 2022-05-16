b0c20157acceeb87652ca6cef2ccf2dea428f8b3
mirko1593 <mirko1593@hotmail.com>
2022-05-16 20:58:27 +0800

    cncf: http service automatic instrumentation

--- cncf/http/main.go
+++ cncf/http/main.go
@@ -11,12 +11,14 @@ import (
 	"net/http"
 	"os"
 
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/baggage"
 	"go.opentelemetry.io/otel/exporters/jaeger"
 	"go.opentelemetry.io/otel/propagation"
 	semconv "go.opentelemetry.io/otel/semconv/v1.10.0"
+	"go.opentelemetry.io/otel/trace"
 
 	"go.opentelemetry.io/otel/sdk/resource"
 	tracesdk "go.opentelemetry.io/otel/sdk/trace"
@@ -63,11 +65,99 @@ func main() {
 
 	otel.SetTracerProvider(tp)
 
-	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
+	otel.SetTextMapPropagator(
+		propagation.NewCompositeTextMapPropagator(
+			propagation.TraceContext{},
+			propagation.Baggage{},
+		),
+	)
 
 	http.HandleFunc("/liveness", handlers.Liveness)
 
 	http.HandleFunc("/fib", handlers.HandleFib)
+
+	otelServerHandler := otelhttp.NewHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		span := trace.SpanFromContext(ctx)
+		bag := baggage.FromContext(ctx)
+		span.AddEvent(
+			"handling hello request...",
+			trace.WithAttributes(
+				attribute.Key("username").String(
+					bag.Member("username").Value(),
+				),
+			),
+		)
+
+		bs, _ := io.ReadAll(r.Body)
+		var data struct {
+			Name string
+		}
+		json.Unmarshal(bs, &data)
+
+		var response = struct {
+			TracerID interface{}
+			Data     interface{}
+		}{
+			TracerID: span.SpanContext().TraceID(),
+			Data: map[string]interface{}{
+				"Message": "Hello " + data.Name,
+			},
+		}
+
+		d, _ := json.Marshal(response)
+
+		w.Write(d)
+
+	}), "Hello Server")
+
+	http.Handle("/helloserver", otelServerHandler)
+
+	otelClientHandler := otelhttp.NewHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		client := http.Client{
+			Transport: otelhttp.NewTransport(http.DefaultTransport),
+		}
+
+		// by propagation.Baggage{}
+		bag, _ := baggage.Parse("username=donuts")
+		ctx := baggage.ContextWithBaggage(r.Context(), bag)
+
+		// by progagation.TracerContext{}
+		ctx, span := tp.Tracer("hello service").Start(ctx, "hello client")
+		defer span.End()
+
+		var data struct {
+			Port string
+			Name string
+		}
+
+		bs, _ := io.ReadAll(r.Body)
+		json.Unmarshal(bs, &data)
+
+		bs, _ = json.Marshal(map[string]string{
+			"name": data.Name,
+		})
+
+		req, err := http.NewRequestWithContext(ctx, http.MethodPost, "http://localhost:"+data.Port+"/helloserver", bytes.NewReader(bs))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		res, err := client.Do(req)
+		if err != nil {
+			log.Fatal(err)
+		}
+		body, err := io.ReadAll(res.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer res.Body.Close()
+
+		w.Write(body)
+
+	}), "Hello Client")
+
+	http.Handle("/helloclient", otelClientHandler)
 
 	http.HandleFunc("/remote", func(w http.ResponseWriter, r *http.Request) {
 		tr := otel.Tracer("remote")
